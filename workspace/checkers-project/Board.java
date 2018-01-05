b545d660088baa971ddb82c0db2d2774f054f36c
Adam Keener <keener101@gmail.com>
2018-01-04 22:42:56 -0700

    added logic to create red and black pieces on the board

--- workspace/checkers-project/Board.java
+++ workspace/checkers-project/Board.java
@@ -19,9 +19,33 @@ public class Board {
 			}
 		}
 		
+		for(int i = 0; i < 3; i++){		//makes initial black pieces
+			if(i+1 % 2 == 1){			//if odd number row (1st row, 3rd row)
+				for(int j = 1; j < 8; j = j+2){
+					board[i][j].setPiece(new Checker(true));
+				}
+			} else {					//if even number row (2nd row)
+				for(int j = 0; j < 8; j = j+2){
+					board[i][j].setPiece(new Checker(true));
+				}
+			}
+
+		}
 		
-		
+		for(int i = 5; i < 8; i++){		//makes initial red pieces
+			if(i+1 % 2 == 1){			//if odd number row (7th row)
+				for(int j = 1; j < 8; j = j+2){
+					board[i][j].setPiece(new Checker(false));
+				}
+			} else {					//if even number row (6th row, 8th row)
+				for(int j = 0; j < 8; j = j+2){
+					board[i][j].setPiece(new Checker(false));
+				}
+			}
+
+		}
+	}
+
 		
 	}
 
-}
