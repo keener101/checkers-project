a388f9a4f2456998242fb26c03e1223aa718cbc4
Adam Keener <keener101@gmail.com>
2018-01-04 22:16:06 -0700

    added logic for all classes

--- workspace/checkers-project/Board.java
+++ workspace/checkers-project/Board.java
@@ -1,4 +1,27 @@
-
+/**
+ * 
+ * Represents a Checkers board
+ * 
+ * @author Keener
+ *
+ */
 public class Board {
+	
+	private boolean blackTurn;
+	private Space[][] board;
+	
+	public Board(){
+		blackTurn = true;
+		
+		for(int i = 0; i < 8; i++){
+			for(int j = 0; j < 8; j++){
+				new Space();
+			}
+		}
+		
+		
+		
+		
+	}
 
 }
--- workspace/checkers-project/Checker.java
+++ workspace/checkers-project/Checker.java
@@ -1,8 +1,16 @@
-/* 
- *
- *A checker object, parent of King.  
+/**
  * 
+ * This represents a single checker piece.
+ * 
+ * 
+ * @author Keener
+ *
  */
 public class Checker {
-
+	
+	private boolean isBlack;
+	
+	public Checker(boolean isPieceBlack){
+		isBlack = isPieceBlack;
+	}
 }
