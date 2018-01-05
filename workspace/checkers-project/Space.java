696e4adc79af5a541cb192d06bbc2a56a8f8f397
Adam Keener <keener101@gmail.com>
2018-01-04 21:49:05 -0700

    added basic logic for Space

--- workspace/checkers-project/Space.java
+++ workspace/checkers-project/Space.java
@@ -8,5 +8,30 @@
 
 
 public class Space {
+	
+	private Checker piece;
+	
+	public Space() {
+		piece = null;
+		
+	}
+	
+	public Space(Checker thisChecker){
+		piece = thisChecker;
+	}
+	
+	public Checker getPiece() {
+		Checker thisPiece = piece;
+		return thisPiece;
+	}
+	
+	public void setPiece(Checker newPiece){
+		piece = newPiece;
+	}
+	
+	public void movePiece(Space newSpace){
+		newSpace.setPiece(piece);
+		piece = null;
+	}
 
 }
