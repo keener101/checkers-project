03323822c7e28d9bdaa62716ca528e62a167fcc1
Adam Keener <keener101@gmail.com>
2018-01-07 16:17:14 -0700

    Wrote canJump() and canMove() for King

--- workspace/checkers-project/King.java
+++ workspace/checkers-project/King.java
@@ -45,8 +45,81 @@ public class King extends Checker {
 
 	
 	
-	public boolean canJump(){
-		return false;
+	public boolean canJump(Board board){
+
+		boolean retVal = false;
+
+			if(checkerSpace.getRow() > 1){	// only rows that can jump forward
+				if(checkerSpace.getCol() < 2){	//if one of the two leftmost columns, can only jump rightwards
+					
+					//this checks for jumps that are up and to the right for left edges
+					if(board.getSpace(checkerSpace.getRow()-1, checkerSpace.getCol()+1).getPiece() != null 
+							&& board.getSpace(checkerSpace.getRow()-1, checkerSpace.getCol()+1).getPiece().getColor() == Color.BLACK 
+							&& board.getSpace(checkerSpace.getRow()-2, checkerSpace.getCol()+2).getPiece() == null){
+						retVal = true;
+					}
+				} else if (checkerSpace.getCol() > 5){ //if one two rightmost columns, can only jump left
+					
+					//this checks for jumps up and left for right edges
+					if(board.getSpace(checkerSpace.getRow()-1, checkerSpace.getCol()-1).getPiece() != null 
+							&& board.getSpace(checkerSpace.getRow()-1, checkerSpace.getCol()-1).getPiece().getColor() == Color.BLACK 
+							&& board.getSpace(checkerSpace.getRow()-2, checkerSpace.getCol()-2).getPiece() == null){
+						retVal = true;
+					}
+				} else { //if somewhere in middle 4 rows, can jump to both left and right
+					
+					//this checks for jumps up and right for middle
+					if(board.getSpace(checkerSpace.getRow()-1, checkerSpace.getCol()+1).getPiece() != null 
+							&& board.getSpace(checkerSpace.getRow()-1, checkerSpace.getCol()+1).getPiece().getColor() == Color.BLACK 
+							&& board.getSpace(checkerSpace.getRow()-2, checkerSpace.getCol()+2).getPiece() == null){
+						retVal = true;
+					}
+
+					//this checks for jumps up and left for middle
+					if(board.getSpace(checkerSpace.getRow()-1, checkerSpace.getCol()-1).getPiece() != null 
+							&& board.getSpace(checkerSpace.getRow()-1, checkerSpace.getCol()-1).getPiece().getColor() == Color.BLACK 
+							&& board.getSpace(checkerSpace.getRow()-2, checkerSpace.getCol()-2).getPiece() == null){
+						retVal = true;
+					}
+				}
+			}
+			
+			if(checkerSpace.getRow() < 6){	//only downwards jumps
+				if(checkerSpace.getCol() < 2){	//if one of the two leftmost columns, can only jump rightwards
+					
+					//this checks for jumps down and to the right for left edges
+					if(board.getSpace(checkerSpace.getRow()+1, checkerSpace.getCol()+1).getPiece() != null 
+							&& board.getSpace(checkerSpace.getRow()+1, checkerSpace.getCol()+1).getPiece().getColor() == Color.RED 
+							&& board.getSpace(checkerSpace.getRow()+2, checkerSpace.getCol()+2).getPiece() == null){
+						retVal = true;
+					}
+				} else if (checkerSpace.getCol() > 5){ //if one two rightmost columns, can only jump left
+					
+					//this checks for jumps down and to the left for right edges
+					if(board.getSpace(checkerSpace.getRow()+1, checkerSpace.getCol()-1).getPiece() != null 
+							&& board.getSpace(checkerSpace.getRow()+1, checkerSpace.getCol()-1).getPiece().getColor() == Color.RED 
+							&& board.getSpace(checkerSpace.getRow()+2, checkerSpace.getCol()-2).getPiece() == null){
+						retVal = true;
+					}
+				} else { //if somewhere in middle 4 rows, can jump to both left and right
+					
+					//this checks for jumps down and right for middle
+					if(board.getSpace(checkerSpace.getRow()+1, checkerSpace.getCol()+1).getPiece() != null 
+							&& board.getSpace(checkerSpace.getRow()+1, checkerSpace.getCol()+1).getPiece().getColor() == Color.RED 
+							&& board.getSpace(checkerSpace.getRow()+2, checkerSpace.getCol()+2).getPiece() == null){
+						retVal = true;
+					}
+					
+					//this checks for jumps down and left for middle
+					if(board.getSpace(checkerSpace.getRow()+1, checkerSpace.getCol()-1).getPiece() != null 
+							&& board.getSpace(checkerSpace.getRow()+1, checkerSpace.getCol()-1).getPiece().getColor() == Color.RED 
+							&& board.getSpace(checkerSpace.getRow()+2, checkerSpace.getCol()-2).getPiece() == null){
+						retVal = true;
+					}
+				}
+			}
+
+		return retVal;
 	}
 	
 	public void move(){
