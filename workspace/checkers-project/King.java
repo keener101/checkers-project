4fa27e3624281ea003b2620a30e46c58bf55a3aa
Adam Keener <keener101@gmail.com>
2018-01-07 15:32:11 -0700

    added canJump() for King

--- workspace/checkers-project/King.java
+++ workspace/checkers-project/King.java
@@ -12,9 +12,38 @@ public class King extends Checker {
 	
 	//TODO all of the necessary methods that King should override
 	
-	public boolean canMove(){
-		return false;
-	}
+	public boolean canMove(Board board){			
+		boolean retVal = false;
+
+			if(checkerSpace.getRow() > 0){			//check all upwards moves
+				if(checkerSpace.getCol() == 0){		//if on left edge, can only check right
+					retVal = board.getSpace(checkerSpace.getRow()-1, checkerSpace.getCol()+1).getPiece() == null;
+				} else if (checkerSpace.getCol() == 7){	//if on right edge, can only check right
+					retVal = board.getSpace(checkerSpace.getRow()-1, checkerSpace.getCol()-1).getPiece() == null;
+				} else {		//else, check both left and right and if either are empty, valid move
+					retVal = (board.getSpace(checkerSpace.getRow()-1, checkerSpace.getCol()+1).getPiece() == null 
+							|| board.getSpace(checkerSpace.getRow()-1, checkerSpace.getCol()-1).getPiece() == null);
+				}
+			}
+			
+			if(checkerSpace.getRow() < 7){			//check all downwards moves
+				if(checkerSpace.getCol() == 0){		//if on left edge, can only check right
+					retVal = board.getSpace(checkerSpace.getRow()+1, checkerSpace.getCol()+1).getPiece() == null;
+				} else if (checkerSpace.getCol() == 7){	//if on right edge, can only check right
+					retVal = board.getSpace(checkerSpace.getRow()+1, checkerSpace.getCol()-1).getPiece() == null;
+				} else {		//else, check both left and right and if either are empty, valid move
+					retVal = (board.getSpace(checkerSpace.getRow()+1, checkerSpace.getCol()+1).getPiece() == null 
+							|| board.getSpace(checkerSpace.getRow()+1, checkerSpace.getCol()-1).getPiece() == null);
+				}
+			}
+			
+
+			System.out.println(retVal);
+			return retVal;	
+			
+		}
+
+	
 	
 	public boolean canJump(){
 		return false;
